9dacd167055eba5e2f8f2cdb3850f1bc5c556120
KojelinHD <gerald.hofbauer@icloud.com>
2018-01-20 00:42:59 +0100

    Now handling KeyboardInterrupts

--- client.py
+++ client.py
@@ -13,28 +13,31 @@ s.setblocking(0)
 print('Server is ready, please type your commands:')
 
 
-def main():
-    def check_incoming():
-        while True:
-            try:
-                received = s.recv(4096).decode('utf-8')
-                if received and not received.isspace():
-                    print('Received:', received)
-            except BlockingIOError:
-                # time.sleep(1)
-                pass
+def check_incoming():
+    while True:
+        try:
+            received = s.recv(4096).decode('utf-8')
+            if received and not received.isspace():
+                print('Received:', received)
+        except BlockingIOError:
+            # time.sleep(1)
+            pass
+        except KeyboardInterrupt:
+            break
 
-    def send():
-        sent_commands = 0
-        while True:
+
+def send():
+    sent_commands = 0
+    while True:
+        try:
             print('ID: ' + str(sent_commands) + ': ')
             data_in = sys.stdin.readline().strip('\n')
             message = '{"command": "' + data_in + '", "id": ' + str(sent_commands) + '}'
             s.send(message.encode('utf-8'))
             sent_commands += 1
+        except KeyboardInterrupt:
+            break
 
-    Thread(target=check_incoming).start()
-    Thread(target=send).start()
 
-
-main()
+Thread(target=check_incoming).start()
+Thread(target=send).start()
